06415c57fcccdf67268eb512db1f7b95eecc7ecc
wpandroidios <wpandroidios@gmail.com>
2017-01-05 22:03:36 +0900

    Enable Gello

--- device.mk
+++ device.mk
@@ -99,8 +99,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml
 
 # Browser
-#PRODUCT_PACKAGES += \
-#    Gello
+PRODUCT_PACKAGES += \
+    Gello
 
 # Camera
 PRODUCT_PACKAGES += \
